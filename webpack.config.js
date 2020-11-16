ac4f3241f718448475deaf4b0ce22fc88d283135
Abubakar Siddique <abz.siddique@gmail.com>
2020-11-16 19:06:09 +0000

    chore: Installing style loaders and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,19 @@
 const path = require("path");
 const webpack = require("webpack");
 module.exports = {
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+    
+        ]
+    }
 }
