001818c1ef6c218df23c7a5828ea5fa03321cce6
Abubakar Siddique <abz.siddique@gmail.com>
2020-11-16 22:58:33 +0000

    chore: Install html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -15,5 +16,13 @@ module.exports = {
             }
     
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin (
+            {
+                template: './src/client/views/index.html',
+                filename: './index.html'
+            }
+        )
+    ]
 }
