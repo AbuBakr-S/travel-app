3eafe1e36a8aeeced0d1df50a9b572908cfc6a71
Abubakar Siddique <abz.siddique@gmail.com>
2020-11-29 18:40:25 +0000

    refactor: Import image into JS

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,12 @@
 import { getPlaceName } from './js/main'
 import { performAction } from './js/main'
 
+import img from './assets/hero.jpg'
+
+window.addEventListener('DOMContentLoaded',function(){
+    document.getElementById('backgroundImage').setAttribute('src', img);
+})
+
 import './styles/style.scss'
 
 export { 
